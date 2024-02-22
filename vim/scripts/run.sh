10795963c4f19cb85fb489df111d644dbe3aa3eb
quandm7 <v.quandm7@vinai.io>
2024-02-22 18:52:21 +0700

    update code for github

--- vim/scripts/run.sh
+++ vim/scripts/run.sh
@@ -37,14 +37,15 @@ export OMP_NUM_THREADS=24
 
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --nnodes=1 --rdzv_endpoint 0.0.0.0:8005 --nproc_per_node=4 vim/train.py \
-                                                                                            --exp XL_2_cpe_-1_block \
-                                                                                            --model DiM-XL/2 \
+                                                                                            --exp L_2_linear_block_adain_cpe-1 \
+                                                                                            --model DiM-L/2 \
                                                                                             --datadir ./vim/dataset/celeba-lmdb/ \
                                                                                             --dataset celeba_256 \
-                                                                                            --global-batch-size 64 \
+                                                                                            --global-batch-size 112 \
                                                                                             --lr 1e-4 \
-                                                                                            --epochs 500 \
+                                                                                            --epochs 600 \
                                                                                             --learn-sigma \
                                                                                             --pe-type cpe \
                                                                                             --block-type linear \
+                                                                                            --no-lr-decay \
                                                                                             # --resume results/notfused_trans_B_2_learn_sigma-DiM-B-2/checkpoints/0000025.pth \
