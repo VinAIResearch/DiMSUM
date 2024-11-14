58d73ca7de9817c9bcf2c534479de2a0833274f4
hao-pt <tienhaophung@gmail.com>
2024-11-13 21:26:22 -0500

    rename fourierblockv2 to dctblock

--- dimsum/models_dim.py
+++ dimsum/models_dim.py
@@ -21,11 +21,8 @@ except ImportError:
 
 from einops import rearrange
 from einops.layers.torch import Rearrange
-import torch_dct
 from transport.blurring import dct_2d, idct_2d, dct, idct
 
-from models_dit import FinalLayer, TimestepEmbedder, LabelEmbedder
-from models_dit import get_2d_sincos_pos_embed, modulate
 from switch_mlp import SwitchMLP
 from mlp import GatedMLP
 from scanning_orders import SCAN_ZOO, reverse_permut_np, local_scan, local_reverse
@@ -403,106 +400,6 @@ class DiMBlock(nn.Module):
 
     def allocate_inference_cache(self, batch_size, max_seqlen, dtype=None, **kwargs):
         return self.mixer.allocate_inference_cache(batch_size, max_seqlen, dtype=dtype, **kwargs)
-
-
-class FourierBlock(nn.Module):
-    def __init__(
-        self,
-        dim,
-        length,
-        norm_cls=nn.LayerNorm,
-        dct_size=1,
-    ):
-        super().__init__()
-        self.dim = dim
-        self.dct_size = dct_size
-        self.norm = norm_cls(dim)
-        self.act = nn.SiLU()
-        self.num_blocks = 4
-        self.block_size = dim // self.num_blocks
-        
-        scale = 0.02
-        # self.weight = nn.Parameter(torch.randn((1, length, dim), dtype=torch.float32)*scale)
-        # self.bias = nn.Parameter(torch.randn((1, length, dim), dtype=torch.float32)*scale)
-
-        # self.weight2 = nn.Parameter(torch.randn((1, length, dim), dtype=torch.float32)*scale)
-        # self.bias2 = nn.Parameter(torch.randn((1, length, dim), dtype=torch.float32)*scale)
-
-        self.weight = nn.Parameter(torch.randn((self.num_blocks, self.block_size, self.block_size), dtype=torch.float32)*scale)
-        self.bias = nn.Parameter(torch.randn((self.num_blocks, self.block_size), dtype=torch.float32)*scale)
-
-        self.weight2 = nn.Parameter(torch.randn((self.num_blocks, self.block_size, self.block_size), dtype=torch.float32)*scale)
-        self.bias2 = nn.Parameter(torch.randn((self.num_blocks, self.block_size), dtype=torch.float32)*scale)
-
-    def multiply(self, input, weights):
-        return torch.einsum('...bd,bdk->...bk', input, weights)
-    
-    def forward(self, x, **kwargs):
-        x = self.norm(x)
-        x = rearrange(x, "b l (h d) -> b d l h", h=self.num_blocks)
-        h = dct_2d(x, self.num_blocks, norm='ortho')
-        h = rearrange(h, "b d l h -> b l h d")
-        
-        ### Old: DCT 1D on dim=1
-        # h = dct(rearrange(x, "b l d -> b d l"), norm='ortho')
-        # h = rearrange(h, "b d l -> b l d").contiguous()
-
-        # h = self.act(self.weight * h + self.bias)
-        # h = self.weight2 * h + self.bias2
-
-        h = self.act(self.multiply(h, self.weight) + self.bias)
-        h = self.multiply(h, self.weight2) + self.bias2
-
-        ### OLD: DCT 1D on dim=1
-        # x = idct(rearrange(h, "b l d -> b d l"), norm='ortho').to(dtype=torch.float32)
-        # x = rearrange(x, "b d l -> b l d").contiguous() # [B, L, D]
-
-        h = rearrange(h, "b l h d -> b d l h")
-        x = idct_2d(h, self.num_blocks, norm='ortho').to(dtype=torch.float32)
-
-        # x = rearrange(x, "b l h w -> b l (h w)")
-        x = rearrange(x, "b d l h -> b l (h d)")
-        
-        return x
-
-
-class FourierBlockv11(nn.Module):
-    def __init__(
-        self,
-        dim,
-        length,
-        norm_cls=nn.LayerNorm,
-        dct_size=1,
-    ):
-        super().__init__()
-        self.dim = dim
-        self.dct_size = dct_size
-        self.norm = norm_cls(dim)
-        self.act = nn.SiLU()
-
-        reserve_kernel = dct_size # (dct_size + 1) // 2 # redundancy reduction by a half
-        reserve_size = 1 # (reserve_kernel) ** 2
-        self.reserve_kernel = reserve_kernel
-        self.dct_conv = init_dct_kernel(dim, dct_size, reserve_kernel)
-        self.idct_conv = nn.Sequential(init_idct_kernel(dim, dct_size, reserve_kernel), nn.PixelShuffle(dct_size))
-        
-        self.conv1 = nn.Conv2d(reserve_size * dim, dim, kernel_size=1, stride=1, padding=0, groups=8, bias=True)
-        self.conv2 = nn.Conv2d(dim, reserve_size * dim, kernel_size=1, stride=1, padding=0, groups=8, bias=True)
-
-    def forward(self, x, **kwargs):
-        x = self.norm(x)
-        x = rearrange(x, "b (h w) c -> b c h w", h=int(np.sqrt(x.size(1))))
-        h = self.dct_conv(x)
-        h = rearrange(h, "b (c p1 p2) h w -> b c (h p1) (w p2)", c=self.dim, p1=self.reserve_kernel)
-
-        h = self.act(self.conv1(h))
-        h = self.conv2(h)
-
-        h = rearrange(h, "b c (h p1) (w p2) -> b (c p1 p2) h w", p1=self.reserve_kernel, p2=self.reserve_kernel)
-        x = self.idct_conv(h)
-        x = rearrange(x, "b c h w -> b (h w) c")
-        
-        return x
 
 
 class DiMBlock(nn.Module):
@@ -1141,155 +1038,6 @@ class WaveDiMBlock(nn.Module):
         return self.mixer.allocate_inference_cache(batch_size, max_seqlen, dtype=dtype, **kwargs)
 
 
-class FourierBlockv13(nn.Module):
-    def __init__(
-        self,
-        dim,
-        length,
-        norm_cls=nn.LayerNorm,
-        dct_size=1,
-        num_heads=8
-    ):
-        super().__init__()
-        self.dim = dim
-        self.dct_size = dct_size
-        self.norm = norm_cls(dim)
-        self.act = nn.SiLU()
-
-        reserve_kernel = dct_size # (dct_size + 1) // 2 # redundancy reduction by a half
-        reserve_size = (reserve_kernel) ** 2 # conv
-        self.reserve_kernel = reserve_kernel
-        # self.dct_conv = init_dct_kernel(dim // reserve_size, dct_size, reserve_kernel)
-        # self.idct_conv = nn.Sequential(init_idct_kernel(dim, dct_size, reserve_kernel), nn.PixelShuffle(dct_size))
-
-        self.dct_conv = init_dct_kernel(dim, dct_size, reserve_kernel)
-        self.idct_conv = nn.Sequential(init_idct_kernel(dim, dct_size, reserve_kernel), nn.PixelShuffle(dct_size))
-        
-        self.in_linear = nn.Conv2d(reserve_size * dim, dim, kernel_size=1, stride=1, padding=0, groups=num_heads, bias=True)
-        self.out_linear = nn.Conv2d(dim, reserve_size * dim, kernel_size=1, stride=1, padding=0, groups=num_heads, bias=True)
-
-        # self.in_linear = nn.Conv2d(dim, dim // reserve_size, kernel_size=1, stride=1, padding=0, groups=num_heads, bias=True)
-        # self.out_linear = nn.Conv2d(dim, reserve_size * dim, kernel_size=1, stride=1, padding=0, groups=num_heads, bias=True)
-
-        # self.in_linear = nn.Linear(dim, dim // reserve_size, bias=True)
-        # self.out_linear = nn.Linear(dim, reserve_size * dim, bias=True)
-
-        # self.adaLN_modulation = nn.Sequential(nn.SiLU(), nn.Linear(dim, 3 * dim, bias=True))
-
-        self.to_1d = Rearrange('b c h w -> b (h w) c')
-        self.to_2d = Rearrange('b (h w) c -> b c h w', h=self.reserve_kernel)
-        self.attn = Attention(dim, num_heads=num_heads, qkv_bias=True)
-
-    def forward(self, x, **kwargs):
-        x = rearrange(x, "b (h w) c -> b c h w", h=int(np.sqrt(x.size(1)))).contiguous()
-        # x = self.in_linear(x)
-        h = self.dct_conv(x) # h = self.to_1d(self.dct_conv(x)).contiguous()
-        # h = rearrange(h, "b (c p1 p2) h w -> b c (h p1) (w p2)", c=self.dim, p1=self.reserve_kernel)
-
-        # shift_ssm, scale_ssm, gate_ssm = self.adaLN_modulation(c).chunk(3, dim=1)
-        # h = h + gate_ssm.unsqueeze(1) * self.attn(modulate(h, shift_ssm, scale_ssm))
-
-        h = self.to_1d(self.in_linear(h)).contiguous()
-        h = h + self.attn(self.norm(h))
-        h = self.out_linear(self.to_2d(h).contiguous())
-
-        # h = rearrange(h, "b c (h p1) (w p2) -> b (c p1 p2) h w", p1=self.reserve_kernel, p2=self.reserve_kernel)
-        x = self.idct_conv(h)
-        
-        return self.to_1d(x).contiguous()
-    
-
-class WaveAttention(nn.Module):
-    def __init__(self, dim, 
-        drop_path=0.,
-        sr_ratio=1,
-        num_heads=8,
-        norm_cls=nn.LayerNorm,
-    ):
-        super().__init__()
-        self.num_heads = num_heads
-        head_dim = dim // num_heads
-        self.scale = head_dim**-0.5
-        self.sr_ratio = sr_ratio
-        self.fused_attn = True
-        self.norm = norm_cls(dim)
-        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
-
-        self.dwt = DWT_2D(wave='haar')
-        self.idwt = IDWT_2D(wave='haar')
-        self.reduce = nn.Sequential(
-            nn.Conv2d(dim, dim//4, kernel_size=1, padding=0, stride=1),
-            # nn.BatchNorm2d(dim//4),
-            nn.SiLU(),
-        )
-        self.filter = nn.Sequential(
-            nn.Conv2d(dim, dim, kernel_size=3, padding=1, stride=1, groups=1),
-            # nn.BatchNorm2d(dim),
-            nn.SiLU(),
-        )
-        self.kv_embed = nn.Conv2d(dim, dim, kernel_size=sr_ratio, stride=sr_ratio) if sr_ratio > 1 else nn.Identity()
-        self.q = nn.Linear(dim, dim)
-        self.kv = nn.Sequential(
-            nn.LayerNorm(dim),
-            nn.Linear(dim, dim * 2)
-        )
-        self.proj = nn.Linear(dim+dim//4, dim)
-        # self.apply(self._init_weights)
-
-    # def _init_weights(self, m):
-    #     if isinstance(m, nn.Linear):
-    #         trunc_normal_(m.weight, std=.02)
-    #         if isinstance(m, nn.Linear) and m.bias is not None:
-    #             nn.init.constant_(m.bias, 0)
-    #     elif isinstance(m, nn.LayerNorm):
-    #         nn.init.constant_(m.bias, 0)
-    #         nn.init.constant_(m.weight, 1.0)
-    #     elif isinstance(m, nn.Conv2d):
-    #         fan_out = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-    #         fan_out //= m.groups
-    #         m.weight.data.normal_(0, math.sqrt(2.0 / fan_out))
-    #         if m.bias is not None:
-    #             m.bias.data.zero_()
-
-    def forward(self, x, residual):
-        if residual is None:
-            residual = x
-        else:
-            residual = residual + self.drop_path(x)
-        x = self.norm(residual.to(dtype=self.norm.weight.dtype))
-
-        B, N, C = x.shape
-        H, W = [int(np.sqrt(N))]*2
-        q = self.q(x).reshape(B, N, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)
-
-        x = x.view(B, H, W, C).permute(0, 3, 1, 2)
-        x_dwt = self.dwt(self.reduce(x))
-        x_dwt = self.filter(x_dwt)
-
-        x_idwt = self.idwt(x_dwt)
-        x_idwt = x_idwt.view(B, -1, x_idwt.size(-2)*x_idwt.size(-1)).transpose(1, 2)
-
-        kv = self.kv_embed(x_dwt).reshape(B, C, -1).permute(0, 2, 1)
-        kv = self.kv(kv).reshape(B, -1, 2, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
-        k, v = kv[0], kv[1]
-
-        if self.fused_attn:
-            x = F.scaled_dot_product_attention(
-                q, k, v,
-                dropout_p=0.,
-            )
-        else:
-            attn = (q @ k.transpose(-2, -1)) * self.scale
-            attn = attn.softmax(dim=-1)
-            x = (attn @ v)
-
-        x = x.transpose(1, 2).reshape(B, N, C)
-        x = self.proj(torch.cat([x, x_idwt], dim=-1))
-        return x, residual
-
-
-
-
 class EinFFT(nn.Module):
     def __init__(self, dim):
         super().__init__()
@@ -1331,7 +1079,7 @@ class EinFFT(nn.Module):
         return x
 
 
-class FourierBlockv2(nn.Module):
+class DCTBlock(nn.Module):
     def __init__(
         self,
         dim,
@@ -1763,7 +1511,7 @@ class DiMBlockCombinedFourier(nn.Module):
             c_dim=dim,
         )
 
-        self.freq_mamba = FourierBlockv2(
+        self.freq_mamba = DCTBlock(
             dim//2,
             mixer_cls_2,
             norm_cls=nn.Identity,
@@ -2377,27 +2125,6 @@ class DiM(nn.Module):
             fourier_block_kwargs["zigzag_paths_reverse"] = zz_paths_rev
             self.fourier_blocks = nn.ModuleList(
                 [
-                    # FourierBlock(
-                    #     hidden_size,
-                    #     length=(img_resolution//patch_size)**2,
-                    # )
-
-                    # FourierBlockv13(
-                    #     hidden_size,
-                    #     length=(img_resolution//patch_size)**2,
-                    #     dct_size=dct_size,
-                    # )
-
-                    # WaveAttention(
-                    #     hidden_size,
-                    #     drop_path=dpr[i],
-                    #     norm_cls=partial(nn.LayerNorm, eps=1e-5),
-                    # )
-
-                    # EinFFT(
-                    #     hidden_size,
-                    # )
-
                     WaveDiMBlock(
                         hidden_size,
                         # length=(img_resolution // patch_size)**2, # dct_size*dct_size, 
@@ -2533,7 +2260,7 @@ class DiM(nn.Module):
         freq_residual = None
         for idx, block in enumerate(self.blocks):
             if self.add_before and self.enable_fourier_layers:
-                if isinstance(self.fourier_blocks[idx], FourierBlockv2) or isinstance(self.fourier_blocks[idx], WaveAttention):
+                if isinstance(self.fourier_blocks[idx], DCTBlock):
                     x, freq_residual = self.fourier_blocks[idx](x, freq_residual)
                 elif isinstance(self.fourier_blocks[idx], WaveDiMBlock):
                     x, freq_residual = self.fourier_blocks[idx](x, freq_residual, c)
@@ -2559,7 +2286,7 @@ class DiM(nn.Module):
                 x, residual = block(x, residual, c, inference_params=inference_params)
 
             if not self.add_before and self.enable_fourier_layers:
-                if isinstance(self.fourier_blocks[idx], FourierBlockv2) or isinstance(self.fourier_blocks[idx], WaveAttention):
+                if isinstance(self.fourier_blocks[idx], DCTBlock):
                     x, freq_residual = self.fourier_blocks[idx](x, freq_residual)
                 elif isinstance(self.fourier_blocks[idx], WaveDiMBlock):
                     x, freq_residual = self.fourier_blocks[idx](x, freq_residual, c)
